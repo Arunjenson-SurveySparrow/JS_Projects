9ec9fe4a221c9939c37c188fc9e76544948176f9
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2022-11-21 18:24:48 +0530

    finished counter

---
+++ counter/app.js
@@ -0,0 +1,38 @@
+let value = document.querySelector('#value');
+let increase = document.querySelector('.decrease');
+const decrease = document.querySelector('.increase');
+// let reset = document.querySelector('.reset');
+const btns = document.querySelectorAll('.btn');
+
+let answer = 0;
+
+btns.forEach(function(btn){
+    btn.addEventListener("click" ,function(e){
+        const state = e.currentTarget.classList;
+        if(state.contains('decrease')){
+            answer--;
+            value.textContent = answer;  
+        }
+        else if(state.contains('increase')){
+            console.log("increase")
+            answer++;  
+        }
+        else{
+            answer = 0; 
+           
+        }
+        if(answer>0){
+            value.style.color = "green";
+        }
+        if(answer<0){
+            value.style.color="red";
+        }
+        if(answer === 0){
+            value.style.color="#222"
+        }
+        value.textContent = answer;
+    });
+});
+if(answer >0){
+ 
+}
